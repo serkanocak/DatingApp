ea92e68f49e853a913765741c31a57b3db156b9d
Serkan Ocak <serkanocak@gmail.com>
2024-03-11 15:12:39 +0300

    end of section 15

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,8 +10,9 @@ public class DataContext : DbContext
     }
     public DbSet<AppUser> Users { get; set; }
     public DbSet<UserLike> Likes { get; set; }
+    public DbSet<Message> Messages { get; set; }
 
-       protected override void OnModelCreating(ModelBuilder builder)
+    protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
 
@@ -29,6 +30,18 @@ public class DataContext : DbContext
             .WithMany(l => l.LikedByUsers)
             .HasForeignKey(s => s.TargetUserId)
             .OnDelete(DeleteBehavior.Cascade);
-    }
 
+        builder.Entity<Message>()
+        .HasOne(s => s.Recipient)
+        .WithMany(m => m.MessagesReceived)
+        .OnDelete(DeleteBehavior.Restrict);
+
+                builder.Entity<Message>()
+        .HasOne(s => s.Sender)
+        .WithMany(m => m.MessagesSent)
+        .OnDelete(DeleteBehavior.Restrict);
+
+
+
+    }
 }
